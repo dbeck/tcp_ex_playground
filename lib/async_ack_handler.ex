4aeef7fb8bf9acf44bb6bed8dce2b319456ec3fc
David Beck <dbeck@beckground.hu>
2016-02-06 15:28:45 +0100

    checking queue length and apply back pressure

--- lib/async_ack_handler.ex
+++ lib/async_ack_handler.ex
@@ -12,29 +12,29 @@ defmodule AsyncAck.Handler do
     Process.flag(:trap_exit, true)
     loop(socket, transport, responder_pid)
   end
-  
+
   def calc_skipped([]) do
     0
   end
-  
+
   def calc_skipped([{_, skipped}]) do
     skipped
   end
-  
+
   def calc_skipped([{_, skipped} | rest]) do
     1 + skipped + calc_skipped(rest)
   end
 
   def flush(_, _, []) do
   end
-  
+
   def flush(socket, transport, ack_list) do
     [{id, _ } | _ ] = ack_list
     skipped = calc_skipped(ack_list)
     packet = << id :: binary-size(8), skipped :: little-size(32) >>
     transport.send(socket, packet)
   end
-  
+
   def responder(socket, transport, yet_to_parse, ack_list, packet_count) do
     receive do
       {:message, packet} ->
@@ -54,22 +54,27 @@ defmodule AsyncAck.Handler do
     after
       5 ->
         flush(socket, transport, ack_list)
-        responder(socket, transport, yet_to_parse, [], 0)        
+        responder(socket, transport, yet_to_parse, [], 0)
     end
   end
-      
+
   def loop(socket, transport, responder_pid) do
     case transport.recv(socket, 0, 5000) do
       {:ok, packet} ->
+        {:message_queue_len, length} = :erlang.process_info(responder_pid, :message_queue_len)
+        if( length > 100 )
+        do
+          :timer.sleep(div(length, 100))
+        end
         send responder_pid, {:message, packet}
         loop(socket, transport, responder_pid)
       {:error, :timeout} ->
         shutdown(socket, transport, responder_pid)
       _ ->
         shutdown(socket, transport, responder_pid)
-    end    
+    end
   end
-  
+
   defp shutdown(socket, transport, responder_pid) do
     send responder_pid, {:stop}
     receive do
@@ -81,7 +86,7 @@ defmodule AsyncAck.Handler do
   defp parse(<< >>, << >>, _skipped ) do
     { << >>, {} }
   end
-  
+
   defp parse(<< >>, last_id, skipped ) do
     { << >>, { last_id, skipped } }
   end
@@ -97,7 +102,7 @@ defmodule AsyncAck.Handler do
         { unparsed, {} }
     end
   end
-  
+
   defp parse(packet, last_id, skipped) do
     case packet do
       # TODO : revise this 1MB safeguard against garbage here
